44fbd7dcbd47cf53a22c88a617d15656e58c88ea
Scott Lowe <scottlowe0@gmail.com>
2015-11-09 15:53:31 +0000

    Version to 1.3.0
    Increment version number to 1.3.0.

--- schemer_import.m
+++ schemer_import.m
@@ -112,7 +112,7 @@
 function varargout = schemer_import(fname, inc_bools)
 
 % ------------------------ Parameters -------------------------------------
-SCHEMER_VERSION = 'v1.2.6';
+SCHEMER_VERSION = 'v1.3.0';
 
 % ------------------------ Input handling ---------------------------------
 % ------------------------ Default inputs ---------------------------------
