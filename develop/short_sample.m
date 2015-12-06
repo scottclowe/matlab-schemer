e32b40f9b43fe1e678a1bb3ca49c118e5e0d6df1
Scott Lowe <scott.code.lowe@gmail.com>
2015-12-06 14:08:30 +0000

    Add new short_sample.m which is more concise
    This will be used to make a new version of the logo.

---
+++ develop/short_sample.m
@@ -0,0 +1,15 @@
+function short_sample()
+% Short syntax highlighting sample
+global gbl_value;
+gbl_value = 20
+hghlghtvar = 'a_string_example_here';
+
+%% Title of cell
+badstring = 'unterminated_string;
+unusedvar = hghlghtvar;
+!echo "Example `!` system command"
+for iterval = 1:2:gbl_value
+  % This is a very informative comment
+  fprintf('%s:%d\n',hghlghtvar,iterval);
+end
+end
