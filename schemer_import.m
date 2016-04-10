625fb108d29745b5448ef4c4582ced6f7501170b
Scott Lowe <scott.code.lowe@gmail.com>
2016-04-10 04:50:40 +0100

    REL: Increment version to 1.3.4

--- schemer_import.m
+++ schemer_import.m
@@ -112,7 +112,7 @@
 function varargout = schemer_import(fname, inc_bools)
 
 % ------------------------ Parameters -------------------------------------
-SCHEMER_VERSION = 'v1.3.3';
+SCHEMER_VERSION = 'v1.3.4';
 
 % ------------------------ Input handling ---------------------------------
 % ------------------------ Default inputs ---------------------------------
